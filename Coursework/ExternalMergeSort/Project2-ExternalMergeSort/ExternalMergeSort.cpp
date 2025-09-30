64574d4ef1a1b9151ff531a39662fee70f6b222d
YunXiao <1139529798@qq.com>
2025-09-30 22:06:31 +0800

    外部归并排序——实现了归并排序算法

--- Coursework/ExternalMergeSort/Project2-ExternalMergeSort/ExternalMergeSort.cpp
+++ Coursework/ExternalMergeSort/Project2-ExternalMergeSort/ExternalMergeSort.cpp
@@ -5,6 +5,7 @@
 #include<chrono>
 #include<vector>
 #include<utility>
+#include<cstdio>
 
 
 // 随机数生成
@@ -135,6 +136,112 @@ std::vector<std::string> generateRuns(const std::string intputFile, const int CH
 }
 
 
+// 辅助函数：模拟使用两个输入缓冲区和一个输出缓冲区归并两个文件
+void mergeTwoFiles(std::string& inputFile1, std::string& inputFile2, std::string& outputFile)
+{
+	// 打开文件
+	std::ifstream in1(inputFile1, std::ios::binary);
+	std::ifstream in2(inputFile2, std::ios::binary);
+	std::ofstream out(outputFile, std::ios::binary | std::ios::app);
+
+	// 定义输入/输出缓冲区大小
+	const int BUFFER_SIZE = 1024;	// 4KB/sizeof(int)
+
+	// 创建输入/输出缓冲区
+	std::vector<int> buffer1(BUFFER_SIZE);
+	std::vector<int> buffer2(BUFFER_SIZE);
+	std::vector<int> bufferOut(BUFFER_SIZE);
+
+	// 定义索引，指向应该访问的位置
+	size_t idx1 = 0, idx2 = 0, idxOut = 0;
+
+	// 填充输入缓冲区
+	in1.read(reinterpret_cast<char*>(buffer1.data()), BUFFER_SIZE * sizeof(int));
+	size_t size1 = in1.gcount() / sizeof(int);
+
+	in2.read(reinterpret_cast<char*>(buffer2.data()), BUFFER_SIZE * sizeof(int));
+	size_t size2 = in2.gcount() / sizeof(int);
+
+	// 只要输入缓冲区非空，就继续归并
+	while (idx1 < size1 || idx2 < size2) {
+		// 输出缓冲区满，则进行一次IO
+		if (idxOut == BUFFER_SIZE) {
+			out.write(reinterpret_cast<const char*>(bufferOut.data()), BUFFER_SIZE * sizeof(int));
+			idxOut = 0;
+		}
+
+		// 若输入缓冲区2耗尽，只能从缓冲区1加载
+		// 若缓冲区1元素小于缓冲区2元素，从缓冲区1加载
+		if (idx2 >= size2 || (idx1 < size1 && buffer1[idx1] < buffer2[idx2])) {
+			bufferOut[idxOut++] = buffer1[idx1++];
+
+			// 更新输入缓冲区
+			if (idx1 >= size1) {
+				in1.read(reinterpret_cast<char*>(buffer1.data()), BUFFER_SIZE * sizeof(int));
+				size1 = in1.gcount() / sizeof(int);
+				idx1 = 0;
+			}
+		}
+		else {
+			bufferOut[idxOut++] = buffer2[idx2++];
+
+			// 更新输入缓冲区
+			if (idx2 >= size2) {
+				in2.read(reinterpret_cast<char*>(buffer2.data()), BUFFER_SIZE * sizeof(int));
+				size2 = in2.gcount() / sizeof(int);
+				idx2 = 0;
+			}
+		}
+	}
+
+	// 将输出缓冲区未写入的剩余数据写入输出文件
+	if (idxOut > 0)
+		out.write(reinterpret_cast<const char*>(bufferOut.data()), idxOut * sizeof(int));
+
+	// 关闭文件
+	in1.close();
+	in2.close();
+	out.close();
+}
+
+// 顺串归并函数
+void mergeRuns(std::vector<std::string>& runFiles, const std::string& finalResultFile)
+{
+	// 记录归并趟数，用于临时顺串文件命名
+	int passCount = 0;
+
+	// 直到归并成一个文件
+	while (runFiles.size() > 1) {
+		std::vector<std::string> nextPassRunFiles;
+
+		for (int i = 0; i < runFiles.size(); i += 2) {
+			// 偶数个待归并文件
+			if (i + 1 < runFiles.size()) {
+				std::string mergedFileName = "pass_" + std::to_string(++passCount) + "_merge_" + std::to_string(i / 2) + ".tem";
+				mergeTwoFiles(runFiles[i], runFiles[i + 1], mergedFileName);
+				nextPassRunFiles.push_back(mergedFileName);
+			}
+			// 奇数个待归并文件，直接晋级下一轮归并
+			else {
+				nextPassRunFiles.push_back(runFiles[i]);
+			}
+		}
+
+		// 清理临时顺串文件
+		for (const std::string& runName : runFiles) {
+			std::remove(runName.c_str());
+		}
+
+		// 更新顺串文件
+		runFiles = nextPassRunFiles;
+	}
+
+	// 重命名最终的结果文件
+	if (!runFiles.empty())
+		std::rename(runFiles[0].c_str(), finalResultFile.c_str());
+}
+
+
 int main()
 {
 	std::string testFile = "test_file";
@@ -146,19 +253,21 @@ int main()
 	generateTestFile(testFile, recordNum);
 	std::cout << "Successfully generate testfile: ";
 
-	//// 快排算法测试
-	//std::vector<int> testArr = { 2,3,1,4,6,5,9,7,0,8 };
+	/*
+	// 快排算法测试
+	std::vector<int> testArr = { 2,3,1,4,6,5,9,7,0,8 };
 
-	//std::cout << "origin: ";
-	//for (int ele : testArr)
-	//	std::cout << ele << " ";
+	std::cout << "origin: ";
+	for (int ele : testArr)
+		std::cout << ele << " ";
 
-	//quickSort(testArr, 0, testArr.size() - 1);
+	quickSort(testArr, 0, testArr.size() - 1);
 
-	//std::cout << std::endl;
-	//std::cout << "result: ";
-	//for (int ele : testArr)
-	//	std::cout << ele << " ";
+	std::cout << std::endl;
+	std::cout << "result: ";
+	for (int ele : testArr)
+		std::cout << ele << " ";
+	*/
 
 	// 顺串生成
 	std::cout << "Start generating runsfile... " << std::endl;
@@ -169,9 +278,22 @@ int main()
 	std::cout << std::endl;
 
 	// 归并排序
+	const std::string resultFileName = "sort_output.bin";
+	mergeRuns(runsFileNames, resultFileName);
 
+	// 检查部分归并文件结果
+	const int CHECK_SIZE = 100000;
+	std::vector<int> checkArr(CHECK_SIZE);
 
+	std::ifstream in(resultFileName, std::ios::binary);
+	in.read(reinterpret_cast<char*>(checkArr.data()), CHECK_SIZE * sizeof(int));
+	int realSize = in.gcount() / sizeof(int);
 
-
+	std::cout << "Final result: " << std::endl;
+	for (int i = 0; i < realSize;i ++) {
+		std::cout << checkArr[i] << ", ";
+		if (i > 0 && i % 10 == 0)
+			std::cout << std::endl;
+	}
 	return 0;
 }
