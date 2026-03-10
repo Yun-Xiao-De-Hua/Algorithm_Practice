1eadd45861e0c6f12d162942e2cb742a660b12d2
YunXiao <1139529798@qq.com>
2026-03-10 22:03:52 +0800

    时间复杂度优化-luogu-p10429

--- Enumerate/luogu-p10429/main.cpp
+++ Enumerate/luogu-p10429/main.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 #include<climits>
 
 const int N = 1e3 + 10;
@@ -41,6 +41,55 @@ int main()
 		}
 	}
 
+	std::cout << ans;
+
+	return 0;
+}*/
+
+#include<iostream>
+#include<climits>
+#include<set>
+
+const int N = 1e3 + 10;
+long long a[N];
+int n, ans = INT_MAX;
+std::multiset<long long> S;
+
+int min(int a, long long b) {
+	return a < b ? a : b;
+}
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0), std::cout.tie(0);
+
+	std::cin >> n;
+	for (int i = 1; i <= n; i++) {
+		std::cin >> a[i];
+		a[i] = a[i - 1] + a[i];
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = i; j <= n; j++) {
+			S.insert(a[j] - a[i - 1]);
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = i; j <= n; j++) {
+			auto p = S.find(a[j] - a[i - 1]);
+			S.erase(p);
+		}
+
+		for (int j = 1; j <= i; j++) {
+			long long suml = a[i] - a[j - 1];
+			auto p = S.lower_bound(suml);
+			if (p != S.end()) ans = min(ans, abs(*p - suml));
+			if (p != S.begin()) ans = min(ans, abs(*(--p) - suml));
+		}
+	}
+	
 	std::cout << ans;
 
 	return 0;
