4f8de521ae8b5ac9a4bfb9a580bf1126cf1e3fc2
YunXiao <1139529798@qq.com>
2026-03-10 16:52:53 +0800

    优化了代码处理-luogu-b3612

--- PrefixSum/luogu-b3612/main.cpp
+++ PrefixSum/luogu-b3612/main.cpp
@@ -1,22 +1,46 @@
+//#include<iostream>
+//
+//int a[100010],l[100010],r[100010],s[100010];
+//int n, m;
+//
+//int main()
+//{
+//	std::cin >> n;
+//	for (int i = 1; i <= n; ++i) {
+//		std::cin >> a[i];
+//		s[i] += (a[i] + s[i - 1]);
+//	}
+//	std::cin >> m;
+//	for (int i = 1; i <= m; ++i) {
+//		std::cin >> l[i] >> r[i];
+//	}
+//
+//	for (int i = 1; i <= m; ++i) {
+//		std::cout << s[r[i]] - s[l[i]-1] << std::endl;
+//	}
+//
+//	return 0;
+//}
+
 #include<iostream>
 
-int a[100010],l[100010],r[100010],s[100010];
-int n, m;
+int n,m,s[100010];
 
 int main()
 {
+
 	std::cin >> n;
 	for (int i = 1; i <= n; ++i) {
-		std::cin >> a[i];
-		s[i] += (a[i] + s[i - 1]);
-	}
-	std::cin >> m;
-	for (int i = 1; i <= m; ++i) {
-		std::cin >> l[i] >> r[i];
+		int a;
+		std::cin >> a;
+		s[i] = s[i - 1] + a;
 	}
 
+	std::cin >> m;
 	for (int i = 1; i <= m; ++i) {
-		std::cout << s[r[i]] - s[l[i]-1] << std::endl;
+		int l, r;
+		std::cin >> l >> r;
+		std::cout << s[r] - s[l - 1] << '\n';
 	}
 
 	return 0;
