b42027066a665aa0c23e689d25ec14133e0c77fe
YunXiao <1139529798@qq.com>
2026-08-12 22:40:16 +0800

    leetcode-p11

---
+++ String/leetcode-p11/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0, r = height.size() - 1;
+        int ans = 0;
+
+        while (l < r) {
+            int now = min(height[l], height[r]) * (r - l);
+            ans = max(ans, now);
+            if (height[l] < height[r]) l++;
+            else r--;
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    return 0;
+}
